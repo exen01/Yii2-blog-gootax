d965d0660beacf3e53ab76a35cbb59f514ac8638
exen <kirill118reg@gmail.com>
2022-08-02 14:03:50 +0400

    User authentication
    - added validation password function
    - added implementation of IdentityInterface methods

--- models/User.php
+++ models/User.php
@@ -3,6 +3,10 @@
 namespace app\models;
 
 use Yii;
+use yii\base\Exception;
+use yii\db\ActiveQuery;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
 
 /**
  * This is the model class for table "{{%user}}".
@@ -12,10 +16,11 @@ use Yii;
  * @property string $password
  * @property string $email
  * @property string|null $profile
+ * @property string auth_key
  *
  * @property Post[] $posts
  */
-class User extends \yii\db\ActiveRecord
+class User extends ActiveRecord implements IdentityInterface
 {
     /**
      * {@inheritdoc}
@@ -54,10 +59,84 @@ class User extends \yii\db\ActiveRecord
     /**
      * Gets query for [[Posts]].
      *
-     * @return \yii\db\ActiveQuery
+     * @return ActiveQuery
      */
     public function getPosts()
     {
         return $this->hasMany(Post::className(), ['author_id' => 'id']);
     }
+
+    /**
+     * Finds an identity by the given ID.
+     *
+     * @param string|int $id the ID to be looked for
+     * @return IdentityInterface|null the identity object that matches the given ID.
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+
+    /**
+     * Finds an identity by the given token.
+     *
+     * @param string $token the token to be looked for
+     * @return IdentityInterface|null the identity object that matches the given token.
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return static::findOne(['access_token' => $token]);
+    }
+
+    /**
+     * @return int|string current user ID
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string current user auth key
+     */
+    public function getAuthKey()
+    {
+        return $this->auth_key;
+    }
+
+    /**
+     * @param string $authKey
+     * @return bool if auth key is valid for current user
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->getAuthKey() === $authKey;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function beforeSave($insert)
+    {
+        if (parent::beforeSave($insert)) {
+            if ($this->isNewRecord) {
+                $this->auth_key = Yii::$app->security->generateRandomString();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Validates the password.
+     *
+     * @param string $password the password currently being validated.
+     */
+    public function validatePassword($password)
+    {
+        $hash = $this->password;
+
+        return Yii::$app->getSecurity()->validatePassword($password, $hash);
+    }
+
 }
